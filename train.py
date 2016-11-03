49250f7380904712f668f56b45ba0ccefc134b76
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-03 00:13:36 -0500

    Updates the training script

--- train.py
+++ train.py
@@ -1,11 +1,18 @@
+import sys
 from harpreif.agent import Agent
 
-image_dir = './imagenet'
+# python train.py './train' './val' './' 2 8
+args = sys.argv
 
-grid_dim = 2
+train_dir = args[1]
+val_dir = args[2]
+
+checkpoint_dir = args[3]
+
+grid_dim = int(args[4])
 
 num_actions = grid_dim ** 4
-num_gradients = 8
+num_gradients = int(args[5])
 
 agent = Agent(num_actions, grid_dim, num_gradients)
-agent.play_game(image_dir)
+agent.play_game(train_dir, val_dir, checkpoint_dir)
