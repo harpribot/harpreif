e7a576ea18e573ca43a332baa29fb95a6266e756
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-07 00:00:51 -0600

    Adds the representation testing script

---
+++ test.py
@@ -0,0 +1,26 @@
+import sys
+from rep_viz.representation import NearestNeighbour
+from rep_viz.state_indexer import  Image2Feature
+
+args = sys.argv
+
+test_dir = args[1]
+checkpoint_dir = args[2]
+
+grid_dim = int(args[3])
+
+num_actions = grid_dim ** 4
+num_gradients = int(args[4])
+
+im2f = Image2Feature(test_dir, checkpoint_dir, num_actions, num_gradients)
+
+image2feature_map, feat_sz = im2f.image2feature()
+
+nb_obj = NearestNeighbour(image2feature_map, feat_sz)
+nb_obj.save_nearest_neighbors()
+
+
+
+
+
+
